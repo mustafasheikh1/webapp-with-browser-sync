e4a5e94587dfa95d3cda99f88c3ec928ed7f2903
shykh <mustafaahmed726@gmail.com>
2019-02-12 13:17:59 +0500

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ var browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
     browserSync.init({
-      server: './src/templates'
+      server: './src'
     });
 
     gulp.watch("src/css/*.css").on('change', browserSync.reload);
     gulp.watch("src/js/*.js").on('change',browserSync.reload);
     gulp.watch("src/templates/*.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 
